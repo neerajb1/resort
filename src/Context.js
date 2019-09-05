a39d1e31195509c98bbd9802a347d10ef824eb22
Neeraj Shukla <neeraj.shukla@techtreeit.com>
2019-09-05 18:11:10 +0530

    Single room setup

--- src/Context.js
+++ src/Context.js
@@ -25,15 +25,22 @@ class RoomProvider extends Component {
     let tempItems = items.map(item => {
       let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
+
       let room = { ...item.fields, images: images, id: id };
       return room;
     });
     return tempItems;
   }
 
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug == slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );

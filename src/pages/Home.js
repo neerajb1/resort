34a2ecfa243c1a99a80a6f69cb1d670cd8721a9e
Neeraj Shukla <neeraj.shukla@techtreeit.com>
2019-09-05 18:41:53 +0530

    styled hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
+import StyledHero from "../components/StyledHero";
 import { Link } from "react-router-dom";
 
 export default function Home() {
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../Context";
-
+import StyledHero from "../components/StyledHero";
 export default class SingleRoom extends Component {
   constructor(props) {
     super(props);
@@ -42,13 +42,13 @@ export default class SingleRoom extends Component {
       images
     } = room;
     return (
-      <Hero hero="roomsHero">
+      <StyledHero img={images[0]}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             back to rooms{" "}
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }
